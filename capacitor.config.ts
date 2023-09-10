f756af5861c5c1b2faf9e47bd453a2b7b4e0103c
Walid Yacine MAZED <w.mazed@gmail.com>
2023-09-10 14:11:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.slickpay.demo.ionic',
+  appName: 'slickpay-demo-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
